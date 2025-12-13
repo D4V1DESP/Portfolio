e706a5c0f950bcd76a8b0c5a17a8273e003bbc04
D4V1DESP <David.Gomez31@alu.uclm.es>
2025-12-13 17:21:20 +0100

    Projects Section

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,9 @@ import About from '../components/About'
 import Education from '../components/Education';
 import Certifications from '../components/Certifications';
 import Skills from '../components/Skills';
+import Projects from '../components/Projects';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons';
-import { faCopy } from '@fortawesome/free-solid-svg-icons';
 import './App.css'
 
 
@@ -35,28 +35,54 @@ export default function App() {
             </a>
             <a 
               class="py-1 px-2.5 bg-[#2b3137] rounded-lg hover:bg-[#2b3137]/90 hover:scale-105 transition-all duration-200 flex items-center gap-1.5"
-              href="https://github.com/davidgomezescobar"
+              href="https://github.com/D4V1DESP"
               target="_blank"
               rel="noopener noreferrer"
             >
               <FontAwesomeIcon icon={faGithub} className="text-sm" />
               <span>GitHub</span>
             </a>
-            <a 
-              class="py-1 px-2.5 bg-[#BB001B] rounded-lg hover:bg-[#BB001B]/90 hover:scale-105 transition-all duration-200 flex items-center gap-1.5"
-              href="mailto:davidgoes77@gmail.com"
+            <button
+              id="email-button"
+              class="py-1 px-2.5 bg-[#BB001B] rounded-lg hover:bg-[#BB001B]/90 hover:scale-105 transition-all duration-200 flex items-center gap-1.5 cursor-pointer"
+              onClick={async () => {
+                await navigator.clipboard.writeText("davidgoes77@gmail.com");
+                const button = document.getElementById("email-button");
+                button.innerHTML = `<i class="fa-solid fa-copy text-sm"></i><span>Copied!</span>`;
+                setTimeout(() => {
+                  button.innerHTML = `<i class="fa-solid fa-copy text-sm"></i><span>Email</span>`;
+                }, 1000);
+              }}
             >
-              <FontAwesomeIcon icon={faCopy} className="text-sm" />
+              <i class="fa-solid fa-copy text-sm"></i>
               <span>Email</span>
-            </a>
+            </button>
           </div>
         </div>
       </main>
-      <Experience />
-      <About />
-      <Education />
-      <Certifications />
-      <Skills />
+      <section id="experience" class="scroll-mt-24 animate-fade-in-up">
+        <Experience />
+      </section>
+      <section id="about" class="animate-fade-in-up">
+        <About />
+      </section>
+      <section id="education" class="scroll-mt-20 animate-fade-in-up">
+        <Education />
+      </section>
+      <section id="certifications" class="hidden">
+        <Certifications />
+      </section>
+      <section id="skills" class="animate-fade-in-up">
+        <Skills />
+      </section>
+      <section id="projects" class="animate-fade-in-up">
+        <Projects />
+      </section>
+      <a href="https://github.com/D4V1DESP">
+        <h4 class="flex justify-center items-center gap-2 mb-25 text-text-secondary animate-pulse">
+          <i class="devicon-github-plain text-sm"></i>See more projects...
+        </h4>
+      </a>
     </>
   )
 }
